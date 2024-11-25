7ae9bb1a09975fca9b1c3d7428e9ff12c10fd779
Darwin <darwinpatricio_8@hotmail.com>
2024-11-25 17:43:33 +0100

    Stuff

--- app/src/main/java/com/example/basicappforinterview/presentation/videos/components/VideoItem.kt
+++ app/src/main/java/com/example/basicappforinterview/presentation/videos/components/VideoItem.kt
@@ -1,6 +1,5 @@
 package com.example.basicappforinterview.presentation.videos.components
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,23 +7,17 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Call
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
-import coil3.request.ImageRequest
-import coil3.request.crossfade
 import com.example.basicappforinterview.domain.model.Video
 
 @Composable
@@ -32,7 +25,7 @@ fun VideoItem(
     video: Video,
     onClick: () -> Unit = {}
 ) {
-    Card (
+    Card(
         modifier = Modifier
             .padding(8.dp)
             .fillMaxWidth()
@@ -44,10 +37,10 @@ fun VideoItem(
             modifier = Modifier.padding(16.dp),
             verticalAlignment = CenterVertically
         ) {
-            Box (
+            Box(
                 modifier = Modifier
                     .size(100.dp)
-                    .clip(RoundedCornerShape(8.dp)),
+                    .clip(RoundedCornerShape(16.dp)),
             ) {
                 AsyncImage(
                     model = video.thumbnail,
