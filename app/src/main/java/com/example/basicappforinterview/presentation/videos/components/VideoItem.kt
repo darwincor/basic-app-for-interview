68de888a80e82ad2c93b300a7453e0d5a9176202
Darwin <darwinpatricio_8@hotmail.com>
2024-11-29 15:49:14 +0100

    Fix some minor details

--- app/src/main/java/com/example/basicappforinterview/presentation/videos/components/VideoItem.kt
+++ app/src/main/java/com/example/basicappforinterview/presentation/videos/components/VideoItem.kt
@@ -4,9 +4,11 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
@@ -39,7 +41,7 @@ fun VideoItem(
         ) {
             Box(
                 modifier = Modifier
-                    .size(100.dp)
+                    .width(100.dp)
                     .clip(RoundedCornerShape(16.dp)),
             ) {
                 AsyncImage(
@@ -48,16 +50,12 @@ fun VideoItem(
                     clipToBounds = true,
                 )
             }
-
+            Spacer(modifier = Modifier.width(8.dp))
             Column {
                 Text(text = video.title, fontWeight = FontWeight.Bold)
             }
         }
     }
-}
-
-fun crossfade(b: Boolean) {
-
 }
 
 @Preview
